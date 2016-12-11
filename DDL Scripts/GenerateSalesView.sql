56f026a0f42e7577cffb0f7c93c2325ecf2d0960
FAshkani <Fred.Ashkani@gmail.com>
2016-12-10 20:13:54 -0800

    Change in View

--- DDL Scripts/GenerateSalesView.sql
+++ DDL Scripts/GenerateSalesView.sql
@@ -15,14 +15,13 @@ CREATE VIEW [dbo].[v_SalesByProductByCategory]
 AS
 SELECT        sh.SalesOrderID, sh.RevisionNumber, sh.OrderDate, sh.ShipDate, sd.SalesOrderDetailID, sd.CarrierTrackingNumber, sd.OrderQty, sd.SpecialOfferID, sd.UnitPrice, sd.UnitPriceDiscount, sd.LineTotal, 
                          sd.ModifiedDate, pr.ProductID, pr.Name AS Product, pr.ProductNumber, pr.ListPrice, ps.ProductSubcategoryID, ps.Name AS ProductSubCategory, pc.ProductCategoryID, pc.Name AS ProductCategory, 
-                         st.TerritoryID, st.Name AS Territory, st.CountryRegionCode, st.[Group], CASE WHEN month(sh.ShipDate) <= 6 THEN (year(sh.ShipDate)) ELSE year(sh.ShipDate) + 1 END AS finYear, 
+                         CASE WHEN month(sh.ShipDate) <= 6 THEN (year(sh.ShipDate)) ELSE year(sh.ShipDate) + 1 END AS finYear, 
                          sh.SalesOrderNumber
 FROM            Sales.SalesOrderHeader AS sh LEFT OUTER JOIN
                          Sales.SalesOrderDetail AS sd ON sh.SalesOrderID = sd.SalesOrderID LEFT OUTER JOIN
                          Production.Product AS pr ON sd.ProductID = pr.ProductID LEFT OUTER JOIN
                          Production.ProductSubcategory AS ps ON pr.ProductSubcategoryID = ps.ProductSubcategoryID LEFT OUTER JOIN
-                         Production.ProductCategory AS pc ON ps.ProductCategoryID = pc.ProductCategoryID LEFT OUTER JOIN
-                         Sales.SalesTerritory AS st ON sh.TerritoryID = st.TerritoryID
+                         Production.ProductCategory AS pc ON ps.ProductCategoryID = pc.ProductCategoryID  
 
 
 GO
